43748943ee7dbe762013b11e657bdb7c0415c92a
Jayanth9710 <68297123+Jayanth9710@users.noreply.github.com>
2024-10-07 21:50:19 +0530

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,8 @@
 let getSettings = () => {
     return {
         // api:"http://localhost:4000"
-         api : "https://e-commerce-payment-redux.herokuapp.com"
+         // api : "https://e-commerce-payment-redux.herokuapp.com"
+         api : "https://e-commerce-redux-payment-backend-jayanth9710s-projects.vercel.app"
         
     }
 }
